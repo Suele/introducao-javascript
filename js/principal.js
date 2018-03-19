2b95b1d3b40969a97c25ee176ed133a5149b633b
Suelen Guimaraes <suelen.ads.guimaraes@gmail.com>
2018-03-18 23:31:35 -0300

    Adicionado a pasta exercicios.

--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,7 @@
 var tituloPrincipal = document.querySelector('.titulo');
 tituloPrincipal.textContent = 'Aparecida Nutricionista';
 
-// Busca uma lista de pacientes
+// Retorna um array de pacientes
 var pacientes = document.querySelectorAll(".paciente");
 
 for (var i = 0; i < pacientes.length; i++) {
@@ -12,27 +12,32 @@ for (var i = 0; i < pacientes.length; i++) {
   var tdAltura = pacientes[i].querySelector('.info-altura');
   var altura = tdAltura.textContent;
 
+  var tdIMC = pacientes[i].querySelector('.info-imc');
+  var imc = tdIMC.textContent;
+
   var pesoEValido = true;
   var alturaEValida = true;
 
   // Verifica o peso se é valido.
   if ((peso <= 0) || (peso >= 400)) {
     pesoEValido = false;
-    tdPesso.textContent = 'Pesso inválido'
+    tdPesso.textContent = 'Pesso inválido';
+    pacientes[i].classList.add("paciente-invalido");
   }
 
   // Verifica a altura se é valida.
   if (altura <= 0 || altura > 2.90) {
     alturaEValida = false;
     tdAltura.textContent = 'Altura inválida.';
+    pacientes[i].classList.add("paciente-invalido");
   }
 
   // Se a altura e o peso forem validos então
   // calculo do IMC sera feito.
-  if (alturaEValida && peso) {
-    var imc = (peso / (altura * altura));
-    var totalIMC = pacientes[i].querySelector('.info-imc');
-    totalIMC.textContent = imc.toFixed(2);
+  if (alturaEValida && pesoEValido) {
+    var totalIMC = (peso / (altura * altura));
+    var imc = pacientes[i].querySelector('.info-imc');
+    imc.textContent = totalIMC.toFixed(2);
   }
 
 }
