6fda3e629ccba7db36ef4876fd414efd2a293179
Suele <suelen.ads.guimaraes@gmail.com>
2018-04-01 11:05:57 -0300

    Atualização das variaveis para os novos padrões do javascript

--- js/principal.js
+++ js/principal.js
@@ -1,43 +1,41 @@
-var tituloPrincipal = document.querySelector('.titulo');
-tituloPrincipal.textContent = 'Aparecida Nutricionista';
+let tituloPrincipal = document.querySelector('.titulo')
+tituloPrincipal.textContent = 'Aparecida Nutricionista'
 
 // Retorna um array de pacientes
-var pacientes = document.querySelectorAll(".paciente");
+let pacientes = document.querySelectorAll('.paciente')
 
-for (var i = 0; i < pacientes.length; i++) {
+for (let i = 0; i < pacientes.length; i++) {
+  let tdPesso = pacientes[i].querySelector('.info-peso')
+  let peso = tdPesso.textContent
 
-  var tdPesso = pacientes[i].querySelector('.info-peso');
-  var peso = tdPesso.textContent;
+  let tdAltura = pacientes[i].querySelector('.info-altura')
+  let altura = tdAltura.textContent
 
-  var tdAltura = pacientes[i].querySelector('.info-altura');
-  var altura = tdAltura.textContent;
+  let tdIMC = pacientes[i].querySelector('.info-imc')
+  let imc = tdIMC.textContent
 
-  var tdIMC = pacientes[i].querySelector('.info-imc');
-  var imc = tdIMC.textContent;
-
-  var pesoEValido = true;
-  var alturaEValida = true;
+  let pesoEValido = true
+  let alturaEValida = true
 
   // Verifica o peso se é valido.
   if ((peso <= 0) || (peso >= 400)) {
-    pesoEValido = false;
-    tdPesso.textContent = 'Pesso inválido';
-    pacientes[i].classList.add("paciente-invalido");
+    pesoEValido = false
+    tdPesso.textContent = 'Pesso inválido'
+    pacientes[i].classList.add('paciente-invalido')
   }
 
   // Verifica a altura se é valida.
   if (altura <= 0 || altura > 2.90) {
-    alturaEValida = false;
-    tdAltura.textContent = 'Altura inválida.';
-    pacientes[i].classList.add("paciente-invalido");
+    alturaEValida = false
+    tdAltura.textContent = 'Altura inválida.'
+    pacientes[i].classList.add('paciente-invalido')
   }
 
   // Se a altura e o peso forem validos então
   // calculo do IMC sera feito.
   if (alturaEValida && pesoEValido) {
-    var totalIMC = (peso / (altura * altura));
-    var imc = pacientes[i].querySelector('.info-imc');
-    imc.textContent = totalIMC.toFixed(2);
+    let totalIMC = (peso / (altura * altura))
+    let imc = pacientes[i].querySelector('.info-imc')
+    imc.textContent = totalIMC.toFixed(2)
   }
-
 }
